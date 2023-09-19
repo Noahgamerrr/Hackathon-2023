eee08cd1f59e6c283f65e0c08a6651bb24951d78
Noahgamerrr <noah.arsic@gmx.at>
2023-09-19 14:03:30 +0200

    Added Company Prototype

---
+++ src/components/CompanyCard.js
@@ -0,0 +1,17 @@
+function CompanyCard(props) {
+    return (
+        <div class="company-card">
+            <h1 class="company-name">Company Name</h1>
+            <img src="/logo512.png" class="company-logo" alt=""/>
+            <h3>Desired skills:</h3>
+            <div class="trophies">
+                <img src="/assets/images/trophy.png" class="trophy" alt=""/>
+                <img src="/assets/images/trophy.png" class="trophy" alt=""/>
+                <img src="/assets/images/trophy.png" class="trophy" alt=""/>
+                <img src="/assets/images/trophy.png" class="trophy" alt=""/>
+            </div>
+        </div>
+    )
+}
+
+export default CompanyCard;
